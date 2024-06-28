2d771cc750ca94d850196861d721d965ae37dbd9
Martin Acosta <m@sis.uy>
2024-06-27 21:53:29 -0300

    Se ajustaron los metodos y se limpio el codigo

--- app/Http/Controllers/PublicacionController.php
+++ app/Http/Controllers/PublicacionController.php
@@ -4,106 +4,45 @@ namespace App\Http\Controllers;
 
 use App\Models\Publicacion;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Validator;
 
 class PublicacionController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * 
-     * @param  \Illuminate\Http\Request
-     * @return \Illuminate\Http\Response
-     */
-    public function index(Request $request)
+    public function filtrar(Request $request)
     {
-        // realizar una validacion de todos los campos de publicacion en el request, sin requerimiento
-        $requestValid = Validator::make($request->all(), [
-            'title' => 'string',
-            'content' => 'string',
-            'created_by' => 'exists:users,id',
-        ]);
+        $where = [];
+        if ($request->has('title')) $where[] = ["title", "like", "%" . $request->title . "%"];
+        if ($request->has('content')) $where[] = ["content", "like", "%" . $request->content . "%"];
+        if ($request->has('usuario')) $where[] = ["created_by", "=", $request->usuario];
 
-        if ($requestValid->fails()) {
-            return response()->json($requestValid->errors(), 400);
+        $publicaciones = [];
+        try {
+            $publicaciones = Publicacion::with("created_by")->where($where)->paginate(20);
+        } catch (\Exception $e) {
+            return response()->json(["errors" => $e->getMessage()], 500);
         }
 
-        $fieldsSearch = [];
-        if ($request->has('title')) {
-            $fieldsSearch['title'] = $request->title;
-        }
-        if ($request->has('content')) {
-            $fieldsSearch['content'] = $request->content;
-        }
-        if ($request->has('created_by')) {
-            $fieldsSearch['created_by'] = $request->created_by;
-        }
+        if ($publicaciones->isEmpty()) return response()->json($publicaciones, 404);
 
-        $publicaciones = Publicacion::where($fieldsSearch)->get();
-        if ($publicaciones) {
-            return response()->json($publicaciones, 200);
-        }
-        return response()->json([], 404);
-        
-
-
-
-
-        if ($request->has('created_by')) {
-            $publicaciones = Publicacion::where('created_by', $request->created_by)->get();
-            return response()->json($publicaciones, 200);
-        }
-
-        
-
-
-
-        $publicaciones = Publicacion::all();
-        return response()->json($publicaciones. 200);
+        return response()->json($publicaciones, 200);
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function guardar(Request $request)
     {
-        $requestValid = Validator::make($request->all(), [
-            'title' => 'required|string',
-            'content' => 'required|string',
-            'created_by' => 'required|exists:users,id',
-        ]);
-
-        if ($requestValid->fails()) {
-            return response()->json($requestValid->errors(), 400);
+        try {
+            $publicacion = Publicacion::create([
+                'title' => $request->title,
+                'content' => $request->content,
+                'created_by' => $request->created_by,
+            ]);
+        } catch (\Exception $e) {
+            return response()->json(["errors" => $e->getMessage()], 500);
         }
-        
-        $publicacion = Publicacion::create([
-            'title' => $request->title,
-            'content' => $request->content,
-            'created_by' => $request->created_by,
-        ]);
 
-        return response()->json([], 201);
+        return response()->json($publicacion, 201);
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function obtener($id)
     {
-        $idValid = Validator::make(['id' => $id], [
-            'id' => 'required:number',
-        ]);
-
-        if ($idValid->fails()) {
-            return response()->json($idValid->errors(), 400);
-        }
 
         $publicacion = Publicacion::find($id);
         if ($publicacion) {
@@ -112,45 +51,27 @@ class PublicacionController extends Controller
         return response()->json([], 404);
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function actualizar(Request $request, $id)
     {
-        $idValid = Validator::make(['id' => $id], [
-            'id' => 'required:number',
+        $publicacion = Publicacion::find($id);
+        if (!$publicacion) return response()->json([], 404);
+
+        $publicacion->update([
+            'title' => $request->title,
+            'content' => $request->content,
+        
         ]);
-
-        $requestValid = Validator::make($request->all(), [
-            'title' => 'required|string',
-            'content' => 'required|string',
-            'created_by' => 'required|exists:users,id',
-        ]);
-
-        if ($idValid->fails()) {
-            return response()->json($idValid->errors(), 400);
-        }
-
-        if ($requestValid->fails()) {
-            return response()->json($requestValid->errors(), 400);
-        }
-
-       
-        return response()->json([], 404);
+        
+        return response()->json($publicacion, 200);
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function eliminar($id)
     {
-        //
+        $publicacion = Publicacion::find($id);
+        if ($publicacion) {
+            $publicacion->delete();
+            return response(204);
+        }
+        return response()->json([], 404);
     }
 }
