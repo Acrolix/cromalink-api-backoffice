a31c8b55ee41083347429f8ba9243b855a7da89c
Martin Acosta <m@sis.uy>
2024-06-27 21:56:02 -0300

    Se agruparon rutas para mejor entendimiento

--- routes/api.php
+++ routes/api.php
@@ -1,23 +1,12 @@
 <?php
 
 use App\Http\Controllers\PublicacionController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| is assigned the "api" middleware group. Enjoy building your API!
-|
-*/
-
-
-Route::get('/posts', [PublicacionController::class, 'index'])->name('posts.index');
-Route::post('/posts', [PublicacionController::class, 'store'])->name('posts.store');
-Route::get('/posts/{id}', [PublicacionController::class, 'show'])->name('posts.show');
-Route::put('/posts/{id}', [PublicacionController::class, 'update'])->name('posts.update');
-Route::delete('/posts/{id}', [PublicacionController::class, 'destroy'])->name('posts.destroy');
+Route::group(['prefix' => 'publicaciones'], function () {
+    Route::get('/', [PublicacionController::class, 'filtrar'])->name('publicaciones.filtrar');
+    Route::post('/', [PublicacionController::class, 'guardar'])->name('publicaciones.guardar');
+    Route::get('/{id}', [PublicacionController::class, 'obtener'])->name('publicaciones.obtener');
+    Route::put('/{id}', [PublicacionController::class, 'actualizar'])->name('publicaciones.actualizar');
+    Route::delete('/{id}', [PublicacionController::class, 'eliminar'])->name('publicaciones.eliminar');
+});
