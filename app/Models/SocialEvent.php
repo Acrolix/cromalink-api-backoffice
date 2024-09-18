78b0295c48950604a70cce2d43544b23d6ae6eb4
Martín Acosta <m@sis.uy>
2024-09-18 19:52:22 -0300

    integracion con SocialEvent

---
+++ app/Models/SocialEvent.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class SocialEvent extends Model
+{
+    use HasFactory;
+}
